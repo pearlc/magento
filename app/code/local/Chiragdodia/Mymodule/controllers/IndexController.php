7a0821dafd7158f8bdb6b56279730276a665169e
rchung <rchung@kr-rchung1ml1.local>
2015-08-23 18:05:04 +0900

    http://code.tutsplus.com/tutorials/magento-custom-module-development--cms-20643 예제 구현

---
+++ app/code/local/Chiragdodia/Mymodule/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+class Chiragdodia_Mymodule_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "Hello tuts+ World";
+    }
+
+    public function testAction()
+    {
+        echo "test action";
+    }
+}
